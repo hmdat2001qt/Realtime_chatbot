d3d59d4551e22f5d8e837f621f89cb9d3007a16b
hmdat2001qt <124838986+hmdat2001qt@users.noreply.github.com>
2025-04-28 13:56:01 +0700

    Update db.js

--- backend/src/model/db.js
+++ backend/src/model/db.js
@@ -8,6 +8,12 @@ const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 async function testSequelizeConnection() {
